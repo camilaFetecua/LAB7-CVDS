6ef7d8016d5138097b5887e0a563bb3bb7133ffb
Desarik98 <d.mejia98@gmail.com>
2021-03-28 15:14:34 -0500

    Empezando vistas

---
+++ src/main/java/edu/eci/cvds/view/AlquilerItemsBean.java
@@ -0,0 +1,12 @@
+package edu.eci.cvds.view;
+
+import com.google.inject.Inject;
+import edu.eci.cvds.samples.entities.Cliente;
+import edu.eci.cvds.samples.services.ExcepcionServiciosAlquiler;
+import edu.eci.cvds.samples.services.ServiciosAlquiler;
+
+import java.util.List;
+
+public class AlquilerItemsBean extends BasePageBean{
+
+}
