55f2b844aa962dd43b3a9835ef944f8c8c5bbddb
Desarik98 <d.mejia98@gmail.com>
2021-03-28 15:44:12 -0500

    Preparacion punto 3

--- src/main/java/edu/eci/cvds/view/AlquilerItemsBean.java
+++ src/main/java/edu/eci/cvds/view/AlquilerItemsBean.java
@@ -5,8 +5,14 @@ import edu.eci.cvds.samples.entities.Cliente;
 import edu.eci.cvds.samples.services.ExcepcionServiciosAlquiler;
 import edu.eci.cvds.samples.services.ServiciosAlquiler;
 
+import javax.faces.bean.ManagedBean;
+import javax.enterprise.context.ApplicationScoped;
 import java.util.List;
 
+@ManagedBean(name = "AlquilerItemsBean")
+@ApplicationScoped
 public class AlquilerItemsBean extends BasePageBean{
-
+    @Inject
+    private ServiciosAlquiler serviciosAlquiler;
+    private Cliente cliente;
 }
