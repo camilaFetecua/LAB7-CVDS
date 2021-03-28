81725058d8f02556fb74730c3e1e14a3519e27b4
Maria Camila Fetecua <maria.fetecua@mail.escuelaing.edu.co>
2021-03-28 13:50:51 -0500

    Lab08 parte 3 casi terminada

--- src/main/java/edu/eci/cvds/guice/GuiceContextListener.java
+++ src/main/java/edu/eci/cvds/guice/GuiceContextListener.java
@@ -3,6 +3,15 @@ package edu.eci.cvds.guice;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+
+import edu.eci.cvds.sampleprj.dao.ClienteDAO;
+import edu.eci.cvds.sampleprj.dao.ItemDAO;
+import edu.eci.cvds.sampleprj.dao.TipoItemDAO;
+import edu.eci.cvds.sampleprj.dao.mybatis.MyBatisClienteDAO;
+import edu.eci.cvds.sampleprj.dao.mybatis.MyBatisItemDAO;
+import edu.eci.cvds.sampleprj.dao.mybatis.MyBatisTipoItemDAO;
+import edu.eci.cvds.samples.services.ServiciosAlquiler;
+import edu.eci.cvds.samples.services.impl.ServiciosAlquilerItemsImpl;
 import org.mybatis.guice.XMLMyBatisModule;
 import org.mybatis.guice.datasource.helper.JdbcHelper;
 import com.google.inject.Guice;
@@ -24,12 +33,16 @@ public class GuiceContextListener implements ServletContextListener {
                     setClassPathResource("mybatis-config.xml");
 
                     // TODO Add service class associated to Stub implementation
-                    bind(AAA.class).to(YYY.class);
-                    bind(BBB.class).to(ZZZ.class);
+                    bind(ItemDAO.class).to(MyBatisItemDAO.class);
+                    bind(ClienteDAO.class).to(MyBatisClienteDAO.class);
+                    bind(TipoItemDAO.class).to(MyBatisTipoItemDAO.class);
+                    bind(ServiciosAlquiler.class).to(ServiciosAlquilerItemsImpl.class);
+
                 }
             });
 
             servletContextEvent.getServletContext().setAttribute(Injector.class.getName(), injector);
         }
     }
-}
+
+
